11c35db53ab81f338c69d6497257e11bf1a19f30
00WhoIam00 <115895954+00WhoIam00@users.noreply.github.com>
2023-11-03 21:47:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,38 @@
-const maze = document.getElementById('maze');
-const rows = 10;
-const cols = 10;
-const cellSize = 40;
+const maze = document.getElementById("maze");
 
-const mazeGrid = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+const mazeData = [
+  "###################",
+  "#                 #",
+  "# ### ### ### ### #",
+  "# #   #   #   #   #",
+  "# # # # # # # # # #",
+  "# # # # # # # # # #",
+  "# # # # # # # # # #",
+  "#   #   #   #   # #",
+  "# ### ### ### ### #",
+  "#   #           # #",
+  "### # ######### # #",
+  "#   #         #   #",
+  "# ##### ##### ### #",
+  "#     #   #   #   #",
+  "### # # # # # # ###",
+  "#   #   #   # #   #",
+  "# ### # ### # ### #",
+  "#   #   #   #     #",
+  "###################",
 ];
 
-const player = {
-    x: 1,
-    y: 1
-};
-
-function generateMaze() {
-    maze.innerHTML = '';
-
-    for (let y = 0; y < rows; y++) {
-        for (let x = 0; x < cols; x++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-
-            if (mazeGrid[y][x] === 1) {
-                cell.classList.add('wall');
-            }
-
-            if (x === player.x && y === player.y) {
-                cell.classList.add('player');
-            }
-
-            maze.appendChild(cell);
-        }
+function createMaze() {
+  maze.innerHTML = "";
+  for (let row = 0; row < mazeData.length; row++) {
+    for (let col = 0; col < mazeData[row].length; col++) {
+      const cell = document.createElement("div");
+      cell.className = mazeData[row][col] === "#" ? "wall" : "";
+      cell.style.left = col * 30 + "px";
+      cell.style.top = row * 30 + "px";
+      maze.appendChild(cell);
     }
+  }
 }
 
-function movePlayer(dx, dy) {
-    const newX = player.x + dx;
-    const newY = player.y + dy;
-
-    if (mazeGrid[newY][newX] === 0) {
-        player.x = newX;
-        player.y = newY;
-        generateMaze();
-    }
-
-    if (player.x === cols - 2 && player.y === rows - 2) {
-        alert('Congratulations! You reached the end of the maze.');
-    }
-}
-
-generateMaze();
-
-document.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'ArrowUp':
-            movePlayer(0, -1);
-            break;
-        case 'ArrowDown':
-            movePlayer(0, 1);
-            break;
-        case 'ArrowLeft':
-            movePlayer(-1, 0);
-            break;
-        case 'ArrowRight':
-            movePlayer(1, 0);
-            break;
-    }
-});
+createMaze();
