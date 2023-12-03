5372752cdb54c4d439e1a504ce30f3adac276f3c
00WhoIam00 <115895954+00WhoIam00@users.noreply.github.com>
2023-12-03 18:55:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let isBlack = true;
+// Array to store chat messages
+let messages = [];
 
-  function switchColor() {
-    document.body.style.backgroundColor = isBlack ? "white" : "black";
-    isBlack = !isBlack;
-    requestAnimationFrame(switchColor);
-  }
+// Function to display messages in the chat box
+function displayMessages() {
+    const chatBox = document.getElementById('chatBox');
+    chatBox.innerHTML = messages.map(msg => `<p>${msg}</p>`).join('');
+}
 
-  switchColor();
-});
+// Function to send a new message
+function sendMessage() {
+    const messageInput = document.getElementById('messageInput');
+    const message = messageInput.value.trim();
+
+    if (message !== '') {
+        // Add the new message to the array
+        messages.push(message);
+
+        // Display the updated messages
+        displayMessages();
+
+        // Clear the input field
+        messageInput.value = '';
+    }
+}
+
+// Initial display of messages
+displayMessages();
