43296e7fd83661d8ec945a40b6dd8c13e94bf009
00WhoIam00 <115895954+00WhoIam00@users.noreply.github.com>
2023-12-03 18:50:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  let isBlack = true;
 
-function getRandomColor() {
-    // Return either the brightest white or the blackest black
-    return Math.round(Math.random()) === 0 ? '#000000' : '#FFFFFF';
-}
-
-function switchColor() {
-    // Set background color to a random color
-    body.style.backgroundColor = getRandomColor();
-
-    // Schedule the next color switch
+  function switchColor() {
+    document.body.style.backgroundColor = isBlack ? "white" : "black";
+    isBlack = !isBlack;
     requestAnimationFrame(switchColor);
-}
+  }
 
-// Request fullscreen mode
-function requestFullscreen() {
-    if (body.requestFullscreen) {
-        body.requestFullscreen();
-    } else if (body.mozRequestFullScreen) { /* Firefox */
-        body.mozRequestFullScreen();
-    } else if (body.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-        body.webkitRequestFullscreen();
-    } else if (body.msRequestFullscreen) { /* IE/Edge */
-        body.msRequestFullscreen();
-    }
-}
-
-// Start the color switching loop
-requestFullscreen(); // Request fullscreen on page load
-switchColor();
+  switchColor();
+});
