c9bfad0364d1e2c52ff5d7a952f5f92dbf64813a
00WhoIam00 <115895954+00WhoIam00@users.noreply.github.com>
2023-12-03 18:41:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,5 +13,19 @@ function switchColor() {
     requestAnimationFrame(switchColor);
 }
 
+// Request fullscreen mode
+function requestFullscreen() {
+    if (body.requestFullscreen) {
+        body.requestFullscreen();
+    } else if (body.mozRequestFullScreen) { /* Firefox */
+        body.mozRequestFullScreen();
+    } else if (body.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        body.webkitRequestFullscreen();
+    } else if (body.msRequestFullscreen) { /* IE/Edge */
+        body.msRequestFullscreen();
+    }
+}
+
 // Start the color switching loop
+requestFullscreen(); // Request fullscreen on page load
 switchColor();
